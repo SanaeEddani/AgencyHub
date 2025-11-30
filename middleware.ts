ad26154739dabeb087bcedc49517a4914bf18d93
sanae eddani <sanae.eddani@gmail.com>
2025-11-30 20:36:12 +0100

    Fix Prisma configuration for Neon

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-    matcher: ["/api/:path*"],
-});
+// Middleware Clerk
+export default clerkMiddleware();
+
+// Configuration du matcher pour Next.js
+export const config = {
+    matcher: ["/api/:path*"], // protège toutes les routes /api/*
+};
